04edbf63b78b919ba193aa3c702f374fbb227294
Eliseu <esreis123@gmail.com>
2023-10-24 00:49:26 -0300

    Listagem de Produtos e Grifes.

--- src/main/java/com/odin/odinbff/rest/produtos/ProdutoController.java
+++ src/main/java/com/odin/odinbff/rest/produtos/ProdutoController.java
@@ -1,9 +1,14 @@
 package com.odin.odinbff.rest.produtos;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.apache.coyote.Request; 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +25,13 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repository;
 	
+	@GetMapping
+	public List<ProdutoFormRequest>getLista(){
+		return repository.findAll().stream()
+				.map(ProdutoFormRequest::fromModel)
+				.collect(Collectors.toList());
+	}
+	
 	@PostMapping
 	public ResponseEntity salvar(@RequestBody  ProdutoFormRequest request) {
 		Produto produto = request.toModel();
@@ -29,3 +41,4 @@ public class ProdutoController {
 	}
 
 }
+
