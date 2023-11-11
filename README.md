55b056d24debdf3fca509378dac7b456c39bfca4
Anderson Passos Silva <passossilva.and@gmail.com>
2023-11-11 07:30:31 -0300

    add list pageable sale

--- README.md
+++ README.md
@@ -1,23 +1,42 @@
 # SGO Odin
--
+
+
+### Considerações do projeto
+
+Para realizar o pull corretamente é necessário instalar o plugin do [git-lfs](https://git-lfs.com/).
+Depois de instalado execute:
+
+```shell
+> git clone "url"
+
+> cd odin-bff
+
+> git lfs install 
+```
 
 ## Ambiente de desenvolvimento
--
+
 
 O projeto está usando docker para iniciar o banco de dados.
 Para iniciar o container do banco de dados utilize o seguinte comando em um terminal:
+
 #### Linux
+
 ```shell
 $ sudo docker compose up
 ```
+
 #### Windows
 
+Abra o Cmd como administrador e entre na pasta do projeto
+
 ````cmd
-# abra o Cmd como administrador e entre na pasta do projeto
 C:\> cd {C:\diretorio\do\projeto\odin-bff}
+
 C:\diretorio\do\projeto\odin-bff> docker compose up
 ````
-Depois que  de iniciar o container, case seja a primeira execução, crie a tabela chamada `odin` no banco de dados.
+
+Depois que  de iniciar o container, case seja a primeira execução, crie um database chamado `odin`.
 
 Detalhes da conexão local:
 
@@ -25,3 +44,5 @@ Detalhes da conexão local:
 - senha: password
 - host: localhost
 - porta: 3307
+
+
--- src/main/java/com/odin/odinbff/controller/sale/SaleController.java
+++ src/main/java/com/odin/odinbff/controller/sale/SaleController.java
@@ -1,18 +1,19 @@
 package com.odin.odinbff.controller.sale;
 
 import com.odin.odinbff.controller.Api;
+import com.odin.odinbff.model.sale.Sale;
 import com.odin.odinbff.repository.ClientRepository;
 import com.odin.odinbff.repository.ProductRepository;
+import com.odin.odinbff.repository.SaleRepository;
 import com.odin.odinbff.repository.ServiceOrderRepository;
 import com.odin.odinbff.service.SaleService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -21,6 +22,8 @@ public class SaleController {
 
     private final SaleService saleService;
 
+    private final SaleRepository saleRepository;
+
     private final ClientRepository clientRepository;
 
     private final ServiceOrderRepository serviceOrderRepository;
@@ -28,10 +31,12 @@ public class SaleController {
     private final ProductRepository productRepository;
 
     public SaleController(@Autowired final SaleService saleService,
+                          @Autowired final SaleRepository saleRepository,
                           @Autowired final ClientRepository clientRepository,
                           @Autowired final ServiceOrderRepository serviceOrderRepository,
                           @Autowired final ProductRepository productRepository) {
         this.saleService = saleService;
+        this.saleRepository = saleRepository;
         this.clientRepository = clientRepository;
         this.serviceOrderRepository = serviceOrderRepository;
         this.productRepository = productRepository;
@@ -51,5 +56,11 @@ public class SaleController {
                                 .buildAndExpand(sale.getId())
                                 .toUri())
                 .build();
+    }
+
+    @GetMapping
+    @Transactional
+    public Page<Sale> getAllPageable(Pageable pageable) {
+        return saleRepository.findAll(pageable);
     }
 }
