88c6f827c26832411729f801f405284a955371a1
Anderson Passos Silva <passossilva.and@gmail.com>
2023-11-12 12:15:23 -0300

    improvement: faster database statup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73aa9ce8b7088ffcafb7d3be43cb2b6d5b6ae704cd1e981a7eba68e259df055f
-size 151686012
+oid sha256:8a12998260cb98f8a9ff4f6983ad12ebd60e28dc2f7cb343ff6e849ce3e3d200
+size 63159292
