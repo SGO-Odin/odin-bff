8eab02be00c37421819e3e2465d62ca2a159a465
Anderson Passos Silva <passossilva.and@gmail.com>
2023-11-13 13:31:01 -0300

    test cors

--- src/main/java/com/odin/odinbff/configuration/security/SpringSecurityConfig.java
+++ src/main/java/com/odin/odinbff/configuration/security/SpringSecurityConfig.java
@@ -15,13 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -53,19 +48,9 @@ public class SpringSecurityConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("*").allowedOrigins("http://localhost:3000/");
+                registry.addMapping("/**").allowedOrigins("localhost:8080");
             }
         };
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
     @Bean
